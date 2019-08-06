c2f06cdaf60dc3f428e2a73272b8153ebeb9c83a
root <root@DESKTOP-4LAVBTU.localdomain>
2019-08-06 12:03:39 +0200

    initial version. ffill of a DF

---
+++ src/timeserieshelper/tests/test_dataalignment.py
@@ -0,0 +1,45 @@
+import pytest
+from timeserieshelper import dataalignment
+
+@pytest.fixture
+def spark():
+    from pyspark.sql import SparkSession
+    import os
+    import sys
+
+    # ensure the PYSPARK Python version on the workers
+    # matches the Python version on the driver
+    # can be tricky when using virtual envs / conda
+    PYSPARK_PYTHON_WORKER = 'python2'
+    IS_PY2 = sys.version_info < (3,)
+    if not IS_PY2:
+        PYSPARK_PYTHON_WORKER = 'python3'  
+    os.environ['PYSPARK_PYTHON'] = PYSPARK_PYTHON_WORKER
+
+    spark = SparkSession.builder\
+        .master('local[2]')\
+        .appName('Unit Testing')\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    return spark
+
+@pytest.fixture
+def df(spark):
+    import sys
+    from pyspark.sql.window import Window
+    import pyspark.sql.functions as func
+
+    d = [{'ts': 1, 'id_2': 'test'}, {'ts': 2, 'id': 109}, {'ts': 3, 'id_2': 'test2'}, {'ts': 4, 'id': 110, 'id_2': 'test3'}, {'ts': 5},  {'ts': 6, 'id_2': 'test4'}]
+    return spark.createDataFrame(d)
+
+def test_create_sample_df(spark, df):
+    result = dataalignment.fill(df, 'ts', ['id', 'id_2'])
+    
+    #debug
+    df.show()
+    result.show()
+    
+    assert result.count() == df.count()
+
+
+
---
+++ src/timeserieshelper/timeserieshelper/dataalignment.py
@@ -0,0 +1,20 @@
+from pyspark.sql import Window
+from pyspark.sql.functions import last
+
+import sys
+
+def fill(df, time_col, columns_to_fill):
+    temp_df = df
+    for col in columns_to_fill:
+        filled_col = _ffill_windows(temp_df, time_col, col)
+        temp_df = temp_df.withColumn(col, filled_col)
+
+    return temp_df
+
+def _ffill_windows(df, time_col, col_to_fill):   
+  # define the window
+  window = Window.orderBy(time_col)\
+                 .rowsBetween(-sys.maxsize, 0)
+
+  # define the forward-filled column
+  return last(df[col_to_fill], ignorenulls=True).over(window)
