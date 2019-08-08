10fab97cc0f0597de5cd06e676c5cea7553736b1
root <root@DESKTOP-4LAVBTU.localdomain>
2019-08-08 11:36:27 +0200

    refactored ffill strategy implementation

--- src/timeserieshelper/timeserieshelper/dataalignment.py
+++ src/timeserieshelper/timeserieshelper/dataalignment.py
@@ -1,20 +1,17 @@
 from pyspark.sql import Window
 from pyspark.sql.functions import last
-
 import sys
 
 def fill(df, time_col, columns_to_fill, strategy):
-    temp_df = df
-    for col in columns_to_fill:
-        filled_col = strategy(temp_df, time_col, col)
-        temp_df = temp_df.withColumn(col, filled_col)
+  return strategy(df, time_col, columns_to_fill)
 
-    return temp_df
-
-def ffill_windows(df, time_col, col_to_fill):    
-  # define the window
+def ffill_windows(df, time_col, columns_to_fill):    
+  # define the window (and order it by time)
   window = Window.orderBy(time_col)\
                  .rowsBetween(-sys.maxsize, 0)
 
-  # define the forward-filled column
-  return last(df[col_to_fill], ignorenulls=True).over(window)
+  # fill every column and replace columns
+  for col in columns_to_fill:
+    df = df.withColumn(col, last(df[col], ignorenulls=True).over(window))
+  
+  return df
