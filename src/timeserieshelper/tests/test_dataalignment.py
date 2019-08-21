7bb92fdf023e3e35dfd93ed5df10d6c5ebbf3905
root <root@DESKTOP-4LAVBTU.localdomain>
2019-08-21 23:02:16 +0200

    implemented resampling (flint). did refactoring.

--- src/timeserieshelper/tests/test_dataalignment.py
+++ src/timeserieshelper/tests/test_dataalignment.py
@@ -1,5 +1,8 @@
 import pytest
+from pyspark.sql.context import SQLContext
 from timeserieshelper import dataalignment
+
+from ts.flint import *
 
 @pytest.fixture
 def spark():
@@ -18,6 +21,7 @@ def spark():
 
     spark = SparkSession.builder\
         .master('local[2]')\
+        .config('spark.jars', 'lib/flint_0_6_0_databricks.jar')\
         .appName('Unit Testing')\
         .getOrCreate()
     spark.sparkContext.setLogLevel("ERROR")
@@ -28,12 +32,50 @@ def df(spark):
     import sys
     from pyspark.sql.window import Window
     import pyspark.sql.functions as func
+    import datetime
 
-    d = [{'ts': 1, 'id_2': 'test'}, {'ts': 2, 'id': 109}, {'ts': 3, 'id_2': 'test2'}, {'ts': 4, 'id': 110, 'id_2': 'test3'}, {'ts': 5},  {'ts': 6, 'id_2': 'test4'}]
+    d = [{'time': datetime.datetime(1984, 1, 1, 1, 1, 1) ,'id_2': 'test'}, \
+        {'time': datetime.datetime(1984, 1, 1, 1, 1, 2), 'id': 109}, \
+        {'time': datetime.datetime(1984, 1, 1, 1, 1, 3), 'id_2': 'test2'}, \
+        {'time':datetime.datetime(1984, 1, 1, 1,1, 4), 'id': 110, 'id_2': 'test3'}, \
+        {'time': datetime.datetime(1984, 1, 1, 1, 1, 5)},  \
+        {'time': datetime.datetime(1984, 1, 1, 1, 1, 6), 'id_2': 'test4'}]
+        
     return spark.createDataFrame(d)
 
-def test_forwardfill_df_successfully(spark, df):
-    result = dataalignment.fill(df, 'ts', ['id', 'id_2'], strategy=dataalignment.ffill_windows)
+def test_dau_initialization_no_sqlcontext_fails(spark, df):
+    with pytest.raises(TypeError):
+        uat = dataalignment.DataAlignmentUtil()
+
+def test_dau_initialization_wrong_sqlcontext_type_fails(spark, df):
+    with pytest.raises(TypeError):
+        uat = dataalignment.DataAlignmentUtil(spark.sparkContext)
+
+def test_fill_forwardfill_correct_number_of_null_successfully(spark, df):
+    from pyspark.sql.functions import isnan, when, count, col
+
+    uat = dataalignment.DataAlignmentUtil(SQLContext.getOrCreate(spark.sparkContext))
+    result = uat.fill(df, 'time', ['id_2'], strategy=uat.ffill_windows)
+    
+    row_null_count_df = df.select([count(when(col(c).isNull(), c)).alias(c) for c in df.columns]).collect()[0]
+    row_null_count_result = result.select([count(when(col(c).isNull(), c)).alias(c) for c in result.columns]).collect()[0]
+
+    #debug
+    df.show()
+    result.show()
+
+    assert row_null_count_result[0] == 0 # id_2 - filled
+    assert row_null_count_result[0] < row_null_count_df[0] # test df has null values
+
+    assert row_null_count_result[1] == 0 # time - nothing to fill
+    assert row_null_count_result[1] == row_null_count_df[1] # test df doesn't have null values
+
+    assert row_null_count_result[2] > 0 # id - not filled
+    assert row_null_count_result[2] == row_null_count_df[2] # nulls but not filled. same like test df
+
+def test_fill_forwardfill_df_successfully(spark, df):
+    uat = dataalignment.DataAlignmentUtil(SQLContext.getOrCreate(spark.sparkContext))
+    result = uat.fill(df, 'time', ['id', 'id_2'], strategy=uat.ffill_windows)
     
     #debug
     df.show()
@@ -41,5 +83,22 @@ def test_forwardfill_df_successfully(spark, df):
 
     assert result.count() == df.count()
 
+def test_fill_invalid_strategy_fails(spark, df):
+    with pytest.raises(Exception):
+        uat = dataalignment.DataAlignmentUtil(SQLContext.getOrCreate(spark.sparkContext))
+        result = uat.fill(df, 'time', ['id', 'id_2'], strategy=None)
 
+def test_fill_invalid_column_fails(spark, df):
+    with pytest.raises(Exception):
+        uat = dataalignment.DataAlignmentUtil(SQLContext.getOrCreate(spark.sparkContext))
+        result = uat.fill(df, 'time', ['id', 'unknown_column'], strategy=uat.ffill_windows)
+
+def test_resample_dataframe_with_correct_number_of_rows(spark, df):
+    uat = dataalignment.DataAlignmentUtil(SQLContext.getOrCreate(spark.sparkContext))  
+    result = uat.resample(df, 'time', timezone='Europe/Vienna', step_size='500ms', join_tolerance = '180ms')
+ 
+    df.show()
+    result.show()
+
+    assert result.count() == ((df.count()) * 2) - 1
 
--- src/timeserieshelper/timeserieshelper/dataalignment.py
+++ src/timeserieshelper/timeserieshelper/dataalignment.py
@@ -1,17 +1,76 @@
-from pyspark.sql import Window
-from pyspark.sql.functions import last
 import sys
 
-def fill(df, time_col, columns_to_fill, strategy):
-  return strategy(df, time_col, columns_to_fill)
+from pyspark.sql import Window, SQLContext
+from pyspark.sql.functions import last
 
-def ffill_windows(df, time_col, columns_to_fill):    
-  # define the window (and order it by time)
-  window = Window.orderBy(time_col)\
-                 .rowsBetween(-sys.maxsize, 0)
+from ts.flint import *
 
-  # fill every column and replace columns
-  for col in columns_to_fill:
-    df = df.withColumn(col, last(df[col], ignorenulls=True).over(window))
+class DataAlignmentUtil:
+
+  def __init__(self,spark_sql_context):
+    if (spark_sql_context is None) or \
+      (not isinstance(spark_sql_context, SQLContext)):
+      raise TypeError("spark_sql_context must be a Spark SQLContext object")
+
+    self._flintContext = FlintContext(spark_sql_context)
+
+  #=============================
+  # BLOCK: resampling
+  #=============================
+
+  def __createTimeSeriesRDD(self, df, time_col, is_sorted, time_unit):
+    return self._flintContext.read \
+      .option('isSorted', is_sorted) \
+      .option('timeColumn', time_col) \
+      .option('timeUnit', time_unit) \
+      .dataframe(df)
+
+  def __createTimeSlicedDataFrame(self, start_time, end_time, timezone, step_size):
+    return self._flintContext.read.range(start_time, end_time, timezone).clock('uniform', step_size)
+
+  def resample(self, df, \
+    time_col = 'time', timezone = 'UTC', \
+    step_size = '1s', join_tolerance = '1s', 
+    start_time = None, end_time = None):
+    
+    from pyspark.sql import functions as F  
+
+    # if not start/end time has been specified use the 
+    # min/max of the dataframe
+    if start_time is None or end_time is None:
+      row_start_end = df.agg(F.min('time'), F.max('time')).collect()[0]
+      if start_time is None:
+        start_time = row_start_end[0]
+      if end_time is None:
+        end_time = row_start_end[1]
+
+    # create the time slices based on the specified duration and step size
+    # create Flint's timeseries representation
+    ts_timeslices = self.__createTimeSlicedDataFrame(start_time, end_time, timezone, step_size)
+    ts_df = self.__createTimeSeriesRDD(df, time_col = time_col, is_sorted = True, time_unit = 'ms')
+
+    # join the target time slices with the original dataframe
+    # using a tolerance
+    return ts_timeslices.leftJoin(ts_df, tolerance = join_tolerance)
+
+  #=============================
+  # BLOCK: filling 
+  #=============================
+
+  def fill(self, df, time_col, columns_to_fill, strategy):
+    return strategy(df, time_col, columns_to_fill)
+
+  def ffill_windows(self, df, time_col, columns_to_fill):    
+    # define the window (and order it by time)
+    window = Window.orderBy(time_col)\
+                  .rowsBetween(-sys.maxsize, 0)
+
+    # fill every column and replace columns
+    for col in columns_to_fill:
+      df = df.withColumn(col, last(df[col], ignorenulls=True).over(window))
+    
+    return df
+
+
+
   
-  return df
